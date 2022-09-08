14afd9b0193a7fd2427a638cfe0a6b775cb2a2fb
Abhishek <abhishekphadtare1998@gmail.com>
2022-09-08 16:50:51 +0530

    python exercise 1

--- learnpythonex1.py
+++ learnpythonex1.py
@@ -1,13 +1,9 @@
 print("hello world!")
-
 print("hello again")
-
 print("I Like typing this.")
-
 print("This is fun.")
-
 print('Yay1! printing.')
-
 print("I'd much rather you 'not'.")
-
 print('I "said" do not touch this.')
+
+print()
