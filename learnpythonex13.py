0b643badc8bbed5999cb2044df50bc4832ab0fa4
Abhishek <abhishekphadtare1998@gmail.com>
2022-09-12 10:41:32 +0530

    o/p syntax -: learnpythonex13.py apple banana gava

--- learnpythonex13.py
+++ learnpythonex13.py
@@ -1,3 +1,4 @@
+#Parameters, Unpacking, Variables
 form sys import argv
 #read the WYSS section for how to run this
 script, first, second, third = argv
@@ -5,4 +6,10 @@ script, first, second, third = argv
 print("The script is called:",script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
-print("Your third variable is:"third)
+print("Your third variable is:",third)
+
+
+
+
+#output from command line - type -: python learnpythonex13.py apple banana gava
+                                
