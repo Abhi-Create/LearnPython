261d1cc8d6d0cca8057183c9a2e54204c0d04ff4
Abhishek <abhishekphadtare1998@gmail.com>
2022-09-29 19:24:01 +0530

    All format - print() function

--- learnpythonex1.py
+++ learnpythonex1.py
@@ -4,5 +4,5 @@ print("I Like typing this.")
 print("This is fun.")
 print('Yay1! printing.')
 print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+print('I "said" do not touch this')
 
--- learnpythonex44.py
+++ learnpythonex44.py
@@ -1,30 +1,17 @@
-class Other(object):
-    def override(self):
-        print("OTHER override()")
-
-    def implicit(self):
-        print("OTHER implicit()")
-    
-    def altered(self):
-        print("OTHER altered()")
-
-class Child(object):
-    def __init__(self):
-        self.other = Other()
-
-    def implicit(self):
-        self.other.implicit()
-
-    def override(self):
-        print("CHILD override()")
+class Parent(object):
 
     def altered(self):
-        print("CHILD, DEFORE OTHER altered()")
-        self.other.altered()
-        print("CHILD, AFTER OTHER altered()")
+        print("PARENT altered()")
 
+class Child(Parent):
+
+    def altered(self):
+        print("CHILD, BEFORE PARENT altered()")
+        super(Child, self).altered()
+        print("CHILD, AFTER PARENT altered()")
+
+dad = Parent()
 son = Child()
 
-son.implicit()
-son.override()
+dad.altered()
 son.altered()
